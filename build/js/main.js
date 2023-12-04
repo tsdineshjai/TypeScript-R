29c04bfbba3e0702a136399d71163def3e1a0d18
tanishka <tsdineshjai@gmail.com>
2023-12-04 13:34:52 +0530

    mastering typescript

---
+++ build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// types
+//you can mentiion optional proper by appending "?" at the end of the property
+const arsenalWinger = {
+    position: "rw",
+    club: "arsenal",
+};
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+// types
+
+type Footballer = {
+	position: string;
+	club: string;
+	age?: number;
+};
+//you can mentiion optional proper by appending "?" at the end of the property
+
+
+const arsenalWinger: Footballer = {
+	position: "rw",
+	club: "arsenal",
+};
