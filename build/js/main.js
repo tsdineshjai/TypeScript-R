16a10f2136d80fe9f4e62b661ad08173d5fd30f0
tanishka <tsdineshjai@gmail.com>
2023-12-05 18:37:14 +0530

    Class Definitions using TS

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,1 @@
 "use strict";
-/* chapter 4 */
-//enums
-var RankingsEPL;
-(function (RankingsEPL) {
-    RankingsEPL[RankingsEPL["Arsenal"] = 0] = "Arsenal";
-    RankingsEPL[RankingsEPL["Liverpool"] = 1] = "Liverpool";
-    RankingsEPL[RankingsEPL["City"] = 2] = "City";
-    RankingsEPL[RankingsEPL["Villa"] = 3] = "Villa";
-})(RankingsEPL || (RankingsEPL = {}));
-console.log("NumberOneRankGoes to :", RankingsEPL.Arsenal);
--- content.ts
+++ content.ts
@@ -1,9 +1,9 @@
 // types
 
 type Footballer = {
-	position: string,
-	club: string,
-	age?: number,
+	position: string;
+	club: string;
+	age?: number;
 };
 //you can mentiion optional proper by appending "?" at the end of the property
 
@@ -47,3 +47,53 @@ const mixedArr: (string | number | boolean)[] = ["success", 33, true];
 function sum(a: number, b: number) {
 	return a + b;
 }
+
+/* class Implementations  */
+
+interface Footballer {
+	name: string;
+	age: number;
+	play(skill: string): string;
+}
+
+class Saka implements Footballer {
+	name: string;
+	age: number;
+
+	constructor(name: string, age: number) {
+		this.name = name;
+		this.age = age;
+	}
+	play(skills: string) {
+		return `${skills}`;
+	}
+}
+
+/* additional class Definitons using TS */
+
+class Something {
+	public dataset: string[];
+
+	constructor() {
+		this.dataset = [];
+	}
+
+	public get getDataArr(): string[] {
+		return this.dataset;
+	}
+
+	public set setValArr(input: string[]) {
+		if (Array.isArray(input) && input.every((ele) => typeof ele === "string")) {
+			this.dataset = input;
+			return;
+		}
+	}
+}
+
+// const footballerDetails = new Something();
+
+// footballerDetails.dataset = ["eni", "amit"];
+// console.log(footballerDetails.getDataArr);
+// footballerDetails.dataset = ["markdon", "alpha", "sigma"];
+
+// console.log(footballerDetails.getDataArr);
